3f786988688c8e79bc70be14af1d0ef15bb0528d
favio <favio@LAPTOP-61TGIM22>
2022-11-08 13:09:06 -0600

    Add project files.

---
+++ L15_FAGA_1088322 TS/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace L15_FAGA_1088322
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            {
+                int[,] matriz = new int[40, 50];
+                int sumaTotal = 0;
+                Random r = new Random();
+                for (int i = 0; i < 40; i++)
+                {
+                    for (int j = 0; j < 50; j++)
+                    {
+                        matriz[i, j] = r.Next(-150, 25);
+                    }
+                }
+                for (int i = 0; i < 40; i++)
+                {
+                    for (int j = 0; j < 50; j++)
+                    {
+                        Console.Write(" " + matriz[i, j]);
+                        sumaTotal += matriz[i, j];
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("El resultado de toda la matriz es: " + sumaTotal);
+                Console.ReadKey();
+            }
+        }
+    }
+}
